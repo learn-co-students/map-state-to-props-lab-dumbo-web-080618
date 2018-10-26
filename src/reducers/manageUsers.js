d0eb23d1478b3f07346a2875e57da7afb8eb92ab
linh4 <linh44h@gmail.com>
2018-10-25 23:25:43 -0400

    done

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -2,14 +2,14 @@ export default function manageUsers(state = {
   users: [],
 }, action){
   switch (action.type) {
-    case 'ADD_USER':
-      console.log('adding ', action.user);
-      return {
-        ...state,
-        users: [...state.users, action.user]
-      }
+  case 'ADD_USER':
+    console.log('adding ', action.user);
+    return {
+      ...state,
+      users: [...state.users, action.user]
+    }
 
-    default:
-      return state;
+  default:
+    return state;
   }
 };
